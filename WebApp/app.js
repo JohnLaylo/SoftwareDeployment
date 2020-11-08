73d9ddb2c7df80c07ed40f9162d2b4f409451f55
JohnLaylo <109304@fhwn.ac.at>
2020-11-08 19:29:34 +0100

    appinsight

--- WebApp/app.js
+++ WebApp/app.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const appInsights = require("applicationinsights");
+appInsights
+  .setup("8c59d630-c433-4764-a5e7-492c5d1a688a")
+  .setDistributedTracingMode(appInsights.DistributedTracingModes.AI_AND_W3C)
+  .start()
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
